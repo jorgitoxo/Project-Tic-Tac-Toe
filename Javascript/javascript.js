896a78b862e776622d1ddcff773bf0890797ebfa
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2025-06-02 17:39:58 +1200

    Set up main structure code
    Made Gameboard and displayController into modules
    Made a function factory for players

---
+++ Javascript/javascript.js
@@ -0,0 +1,22 @@
+const Gameboard = (function () {
+    const gameboard = [
+        "", "", "",
+        "", "", "",
+        "", "", ""
+    ];
+
+    return { gameboard }
+})();
+
+const displayController = (function () {
+    
+    return {};
+})();
+
+function createPlayer(name, faction) {
+    const name = name;
+    const faction = faction; // there'll be 4 teams Red, Green, Blue and Yellow
+    let score = 0;
+
+    return { name, faction, score };
+}
