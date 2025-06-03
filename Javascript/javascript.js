836beff71382273dcd4e9a9856838513b44ddb65
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2025-06-03 21:12:02 +1200

    Display gameboard on console
    Learned difference between function expression and declaration
    Added comments to help understand workings

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -8,6 +8,7 @@
 
 // TCB: Testing code below
 // DCB: Debugging code below
+// CBUT: Code below used for testing
 
 const Game = (function () {
 
@@ -23,11 +24,30 @@ const Gameboard = (function () {
     const rows = 3;
     const columns = 3;
     const gameboard = [];
+    
+    function Cell () {
+        let token = "";
+
+        const getToken = () => token;
+
+        const addToken = (token) => {
+            
+        }
+
+        return {
+            getToken,
+        }
+    }
+
+    function markToken(token) {
+
+    }
 
     // Build the board!
-    // Create a 2D array that will represent the state of the game board
-    // Row 0 represent the top row
-    // Column 0 represents the left-most colum
+    // Create a 2D array that will represent
+    // the state of the game board
+    // Row 0 represents the top row
+    // Column 0 represents the left-most column
     for (let row = 0; row < rows; row++) {
         gameboard[row] = [];
         for (let col = 0; col < columns; col++) {
@@ -35,26 +55,50 @@ const Gameboard = (function () {
         };
     };
 
-    // Method of getting entire game board, that the UI will eventually need to render it
+    // Method of getting entire game board,
+    // that the UI will eventually need to render it
     const getGameBoard = () => gameboard;
     
-    // Code below used for testing //
-    // Prints board to console, helps seeing how the 2D array is initially built
-    // We have to map each cell's value to be able to see the gameboard on the console.
-    // Why does this work, instead of console.logGameboard.getGameBoard)?
-    const printGameBoard = gameboard.map((row) => row.map((cell) => `${cell}`));
+    // CBUT //
+    // Prints board to console, helps seeing how
+    // the 2D array is initially built
+    // Do we have to map each cell's value
+    // to be able to see the gameboard on the console?
+    // Why does this work, instead of console.log(Gameboard.getGameBoard)?
+    // Is it because what is stored in each cell
+    // is a cell object, so we have to extract
+    // the values inside each cell object?
+    // ```
+    // const printGameBoard = gameboard.map((row) => row.map((cell) => `${cell}`));
+    // ```
+    // => Not quite, as we haven't created a cell object yet
+    // This was not possible because getGameBoard
+    // is a const variable that holds a reference to a function
+    // We need make the call as an object method,
+    // not as an object property -- $ console.log(Gameboard.getGameBoard())
+    // Method: Gameboard.getGameBoard()
+    // vs
+    // Property: Gameboard.getGameBoard
+    const printGameBoard = () => { console.log(gameboard) };
     
     return { getGameBoard, printGameBoard }
 })();
 
 // Needs work
-const Player = (function(name, token) {
-    const name = name;
-    const token = token;
+const Player = (function(name="Jorgito", token="X") {
+    const playerName = name;
+    const playerToken = token;
     let score = 0;
 
-    return { name, token, score };
+    // CBUT
+    const logPlayerName = () => console.log(playerName);
+    const logPlayerToken = () => console.log(playerToken);
+    
+
+    return { playerName, playerToken, score, logPlayerName, logPlayerToken };
 })();
 
 // RUN
-console.log(Gameboard.printGameBoard);
+Gameboard.printGameBoard();
+Player.logPlayerName();
+Player.logPlayerToken();
