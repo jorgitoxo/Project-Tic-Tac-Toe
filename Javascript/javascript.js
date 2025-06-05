fb76f850ff1bfa806fd445a3f3b3f3f6e918f260
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2025-06-05 17:31:09 +1200

    Code cleanup and refactor
    Removed brainstom comments about function expression vs declaration
    Changed printGameBoard to a method
    Changed Player fn fact to use getters for the properties of the object

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -11,7 +11,7 @@
 // CBUT: Code below used for testing
 
 const Game = (function () {
-
+    
     return {}
 })();
 
@@ -40,14 +40,13 @@ const Gameboard = (function () {
     }
 
     // Build the board!
-    // Create a 2D array that will represent
+    // The 2D array that will represent
     // the state of the game board
     // Row 0 represents the top row
     // Column 0 represents the left-most column
     for (let row = 0; row < rows; row++) {
         gameboard[row] = [];
         for (let col = 0; col < columns; col++) {
-            // gameboard[row].push("_");
             gameboard[row].push(Cell());
         };
     };
@@ -55,29 +54,10 @@ const Gameboard = (function () {
     // Method of getting entire game board,
     // that the UI will eventually need to render it
     const getGameBoard = () => gameboard;
-    
-    // CBUT //
-    // Prints board to console, helps seeing how
-    // the 2D array is initially built
-    // Do we have to map each cell's value
-    // to be able to see the gameboard on the console?
-    // Why does this work, instead of console.log(Gameboard.getGameBoard)?
-    // Is it because what is stored in each cell
-    // is a cell object, so we have to extract
-    // the values inside each cell object?
-    // ```
-    // const printGameBoard = gameboard.map((row) => row.map((cell) => `${cell}`));
-    // ```
-    // => Not quite, as we haven't created a cell object yet
-    // This was not possible because getGameBoard
-    // is a const variable that holds a reference to a function
-    // We need make the call as an object method,
-    // not as an object property -- $ console.log(Gameboard.getGameBoard())
-    // Method: Gameboard.getGameBoard()
-    // vs
-    // Property: Gameboard.getGameBoard
-    // const printGameBoard = () => { console.log(gameboard) };
-    const printGameBoard = console.log(gameboard.map((row) => row.map((cell) => cell.getToken())));
+
+    // Show the content of the gameboard object
+    // in the console
+    const printGameBoard = () => console.log(gameboard.map((row) => row.map((cell) => cell.getToken())));
     
     return { getGameBoard, printGameBoard }
 })();
@@ -87,13 +67,12 @@ function Player (name="Slick", token="X") {
     this.name = name;
     this.token = token;
     this.score = 0;
-
-    // CBUT
-    const logPlayerName = () => console.log(this.name);
-    const logPlayerToken = () => console.log(this.token);
     
+    const getName = () => this.name;
+    const getToken = () => this.token;
+    const getScore = () => this.score;
 
-    return { name, token, score, logPlayerName, logPlayerToken };
+    return { getName, getToken, getScore };
 };
 
 // CBUT
@@ -110,4 +89,4 @@ function Player (name="Slick", token="X") {
 
 
 // RUN
-Gameboard.printGameBoard;
+Gameboard.printGameBoard();
